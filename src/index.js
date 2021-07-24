1c5ac31f52318c7265926ce55c9cf5a012445a03
Aymeric <aymericbertrand.pro@gmail.com>
2021-07-24 10:59:28 +0200

    Update config and add images and css to the project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-import './style.css';
 import './remedy.css';
+import './root.css';
+import './hero.css';
+
+const init = (() => {
+
+    const initBody = () => {
+    const div = document.createElement('div');
+
+    const content = document.body.appendChild(div);
+    content.id = "content";
+    }
+
+    return { initBody };
+})();
+
+// init.initBody();
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,35 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    // new HtmlWebpackPlugin({
+    //   title: 'Restaurant',
+    // }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
