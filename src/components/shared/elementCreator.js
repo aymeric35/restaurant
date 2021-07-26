30422965e73481b2662f96705b862b9928cd0319
Aymeric <aymericbertrand.pro@gmail.com>
2021-07-26 20:48:11 +0200

    Improve createElement logic

--- src/components/shared/elementCreator.js
+++ src/components/shared/elementCreator.js
@@ -1,27 +1,9 @@
-export const createElement = (() => {
-    const newHeader = document.createElement('header');
-    const newSection = document.createElement('section');
-    const newFooter = document.createElement('footer');
+export const createElement = (element) => {
+    return document.createElement(element);
+};
 
-    const newH1 = document.createElement('h1');
-    const newH2 = document.createElement('h2');
-    const newH3 = document.createElement('h3');
+export const content = (() => {
+    const id = document.getElementById("content");
 
-    const newDiv = document.createElement('div');
-    const newParagraph = document.createElement('p');
-
-    const newImage = document.createElement('img');
-
-    return {
-        newHeader,
-        newSection,
-        newFooter,
-        newH1,
-        newH2,
-        newH3,
-        newDiv,
-        newParagraph,
-        newImage
-    }
-
+    return { id };
 })();
