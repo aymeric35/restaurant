88db85ee34d50cfb6e004b8302ab892c2ba7a481
Aymeric <aymericbertrand.pro@gmail.com>
2021-07-27 13:21:49 +0200

    Switch from insertAdjacentHTML to a generator

--- src/pages/menu/menu.js
+++ src/pages/menu/menu.js
@@ -2,6 +2,54 @@ import './menu.css';
 import { createElement, content } from "../../components/shared/elementCreator";
 import Menu from "./menu.jpg";
 
+const menuList = (() => {
+
+    const Recipe = (name, ingredients, price) => {
+        return { name, ingredients, price };
+    }
+
+    const marguerita = Recipe('Marguerita', 'Sauce tomate, Fromage, Olives', '6,00 €');
+    const prima = Recipe('Prima', 'Sauce tomate, Fromage, Jambon', '6,00 €');
+    const reine = Recipe('Reine', 'Sauce tomate, Fromage, Jambon, champignons', '7,00 €');
+    const romaine = Recipe('Romaine', 'Sauce tomate, Fromage, Jambon, Œuf, Crème fraîche', '7,50 €');
+    const quatreFromages = Recipe('4 Fromages', 'Sauce tomate, Chèvre, Bleu, Emmental, Fromage', '8,00 €');
+    const oceane = Recipe('Océane', 'Sauce tomate, Noix de Saint-Jacques, Crevettes', '8,50€');
+    const flamiche = Recipe('Flamiche', 'Crème Fraîche, Oignons, Lardons, Fromage', '9,00 €');
+    const quatreSaisons = Recipe('4 Saisons', "Sauce tomate, Jambon épaule, Fonds d'Artichauts, Champignons", '9,50 €');
+    const brestoise = Recipe('Brestoire', 'Sauce tomate, Oignons, Pommes de Terre, Lardons, Jambon Cru', '9,50€');
+    const bretonne = Recipe('Bretonne', 'Sauce tomate, Oignons, Andouille, Fromage, Crème fraîche', '9,50€');
+
+    const generate = () => {
+        const listHolder = document.querySelector(".list-holder");
+        const recipes = [marguerita, prima, reine, romaine, quatreFromages, oceane, flamiche, quatreSaisons, brestoise, bretonne];
+
+        recipes.forEach(recipe => {
+            const container = listHolder.appendChild(createElement('div'));
+            container.classList.add("sec-menu__list");
+
+            const recipeContainer = container.appendChild(createElement('div'));
+            recipeContainer.classList.add("sec-menu__recipe-container");
+
+            const title = recipeContainer.appendChild(createElement('h3'));
+            title.classList.add("sec-menu__recipe-title");
+            title.textContent = recipe.name;
+            const border = recipeContainer.appendChild(createElement('div'));
+            border.classList.add("border");
+            const ingredients = recipeContainer.appendChild(createElement('p'));
+            ingredients.classList.add("sec-menu__recipe-ingredients")
+            ingredients.textContent = recipe.ingredients;
+
+            const priceContainer = container.appendChild(createElement('div'));
+            priceContainer.classList.add("sec-menu__price-container");
+            const price = priceContainer.appendChild(createElement('p'));
+            price.textContent = recipe.price;
+
+        })
+    }
+
+    return { generate };
+})();
+
 export const createMenu = (() => {
     const bodyContent = content.id;
 
@@ -24,113 +72,7 @@ export const createMenu = (() => {
         const menuContainer = section.appendChild(createElement('div'));
         menuContainer.classList.add("container", "list-holder");
 
-        const listContainer = document.querySelector(".list-holder");
-        listContainer.insertAdjacentHTML('afterbegin',
-                `<div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">Marguerita</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Sauce tomate, Fromage, Olives</p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>6,00 €</p>
-            </div>
-        </div>
-        <div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">Prima</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Sauce tomate, Fromage, Jambon</p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>6,00 €</p>
-            </div>
-        </div>
-        <div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">Reine</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Sauce tomate, Fromage, Jambon, champignons</p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>7,00 €</p>
-            </div>
-        </div>
-        <div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">Romaine</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Sauce tomate, Fromage, Jambon, Œuf, Crème fraîche</p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>7,50 €</p>
-            </div>
-        </div>
-        <div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">4 Fromages</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Sauce tomate, Chèvre, Bleu, Emmental, Fromage</p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>8,00 €</p>
-            </div>
-        </div>
-        <div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">Bretonne</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Sauce tomate, Oignons, Andouille, Fromage, Crème fraîche
-                </p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>8,50 €</p>
-            </div>
-        </div>
-        <div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">Flamiche</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Crème Fraîche, Oignons, Lardons, Fromage</p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>9,00 €</p>
-            </div>
-        </div>
-        <div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">4 Saisons</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Sauce tomate, Jambon épaule, Fonds d'Artichauts,
-                    Champignons, Poivrons, Fromage</p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>9,50 €</p>
-            </div>
-        </div>
-        <div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">Brestoise</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Sauce tomate, Oignons, Pommes de Terre, Lardons, Jambon
-                    Cru, Crème Fraîche</p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>9,50 €</p>
-            </div>
-        </div>
-        <div class="sec-menu__list">
-            <div class="sec-menu__recipe-container">
-                <h3 class="sec-menu__recipe-title">Bretonne</h3>
-                <div></div>
-                <p class="sec-menu__recipe-ingredients">Sauce tomate, Oignons, Andouille, Fromage, Crème fraîche
-                </p>
-            </div>
-            <div class="sec-menu__price-container">
-                <p>9,50 €</p>
-            </div>
-        </div>`
-        )
+        menuList.generate();
     }
 
     return { appendSection };
