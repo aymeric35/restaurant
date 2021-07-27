e1e25651d0eb1f7ef75759ab8e86800453420646
Aymeric <aymericbertrand.pro@gmail.com>
2021-07-27 16:29:20 +0200

    Improve appendChild logic

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,21 +5,26 @@ import { menu } from '../menu/navMenu';
 const createHeader = (() => {
 
     const appendHeader = () => {
-        const headerDOM = document.body.insertBefore(createElement('header'), content.id);
+        const headerDOM = createElement('header');
         headerDOM.classList.add("hero");
+        document.body.insertBefore(headerDOM, content.id);
 
-        const heroContainer = headerDOM.appendChild(createElement('div'));
+        const heroContainer = createElement('div');
         heroContainer.classList.add("hero__container");
+        headerDOM.appendChild(heroContainer);
 
-        const heroSubContainer = heroContainer.appendChild(createElement('div'));
+        const heroSubContainer = createElement('div');
         heroSubContainer.classList.add("container", "hero__subcontainer");
+        heroContainer.appendChild(heroSubContainer);
 
-        const h1 = heroSubContainer.appendChild(createElement('h1'));
+        const h1 = createElement('h1');
         h1.textContent = 'Breizh Pizza';
+        heroSubContainer.appendChild(h1);
 
-        const menu = heroSubContainer.appendChild(createElement('div'));
+        const menu = createElement('div');
         menu.id = "menu";
         menu.classList.add("hero__menu");
+        heroSubContainer.appendChild(menu);
 
         menu.insertAdjacentHTML('afterBegin',
         `<svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -27,21 +32,21 @@ const createHeader = (() => {
             stroke-linejoin="round" />
         </svg>`);
 
-        const nav = heroSubContainer.appendChild(createElement('nav'));
+        const nav = createElement('nav');
         nav.classList.add("hero__menu-toggle--hidden");
-        const ul = nav.appendChild(createElement('ul'));
-        const li = ul.append(
-            createElement('li'),
-            createElement('li'),
-            createElement('li')
-        );
-        document.querySelector('li:nth-child(1)').textContent = "Accueil";
-        document.querySelector('li:nth-child(2)').textContent = "Menu";
-        document.querySelector('li:nth-child(3)').textContent = "À Propos";
+        heroSubContainer.appendChild(nav);
+
+        const ul = createElement('ul');
+        nav.appendChild(ul);
+        const liHome = createElement('li');
+        const liMenu = createElement('li');
+        const liAbout = createElement('li');
+        ul.append( liHome, liMenu, liAbout);
+        liHome.textContent = "Accueil";
+        liMenu.textContent = "Menu";
+        liAbout.textContent = "À Propos";
 
     }
-
-
 
     return { appendHeader };
 })();
